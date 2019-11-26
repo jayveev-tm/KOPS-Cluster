e3b2184ecdf658947a831f77944263eeb9b60326
Jayvee Velez <jayvee_velez@trendmicro.com>
2019-11-26 03:11:01 +0000

    Add new file

---
+++ templates/deploy.sh
@@ -0,0 +1,6 @@
+source export.sh
+kops create cluster --zones us-east-2a ${NAME}
+kops replace -f ig.yaml
+kops replace -f cluster.yaml
+kops create secret --name ${NAME} sshpublickey admin -i isimmersion-kp-ohio.pub
+kops update cluster ${NAME} --yes
