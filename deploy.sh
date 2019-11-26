b144ad2bd83c96d3a5b620ec10c420de407edd19
Jayvee Velez <jayvee_velez@trendmicro.com>
2019-11-26 03:45:24 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #Exit immediately if non-zero exit code is returned by a command
-set -e
+#set -e
 
 source export.sh
 kops create cluster --zones us-east-2a ${NAME}
-#kops replace -f ig.yaml
-#kops replace -f cluster.yaml
+kops replace -f ./templates/ig.yaml
+kops replace -f ./templates/cluster.yaml
 kops create secret --name ${NAME} sshpublickey admin -i key.pub
 kops update cluster ${NAME} --yes
